2629f3ae48ceccced641918dc6227583f021eaec
Roland Sebestyen <sebestyenroland21@gmail.com>
2018-12-09 14:56:02 +0200

    added ping route

--- main.go
+++ main.go
@@ -5,14 +5,20 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"fmt"
 	"github.com/eye1994/game-server/ws"
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
+func ping(w http.ResponseWriter, r *http.Request) {
+		 fmt.Fprintf(w, "pong")
+}
+
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
+	http.HandleFunc("/ping", ping)
 	http.HandleFunc("/ws", ws.HandleWsConnection)
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
